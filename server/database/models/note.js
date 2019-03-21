18a715a9b22097ca2c5dd73a194891e0b603695d
Chrismarcel <cmarcelj@gmail.com>
2019-03-21 22:28:12 +0100

    set up babel and eslint

--- server/database/models/note.js
+++ server/database/models/note.js
@@ -1,11 +1,11 @@
-'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const Note = sequelize.define('Note', {
     title: DataTypes.STRING,
     body: DataTypes.STRING,
     UserId: DataTypes.INTEGER
   }, {});
-  Note.associate = function (models) {
+  Note.associate = (models) => {
     Note.belongsTo(models.User);
   };
   return Note;
--- server/database/models/user.js
+++ server/database/models/user.js
@@ -1,11 +1,11 @@
-'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: DataTypes.STRING,
     username: DataTypes.STRING,
     password: DataTypes.STRING
   }, {});
-  User.associate = function (models) {
+  User.associate = (models) => {
     User.hasMany(models.Note);
   };
   return User;
